09f31490aa177fb18c667cad50d6f6be0d2b12fd
Stefano Terragni <stefano.terragni@everis.com>
2022-10-19 19:35:13 +0200

    Initial Commit

---
+++ src/main/java/it/gov/pagopa/notification/email/NotificationEmailApplication.java
@@ -0,0 +1,15 @@
+package it.gov.pagopa.notification.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NotificationEmailApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationEmailApplication.class, args);
+  }
+
+}
+
