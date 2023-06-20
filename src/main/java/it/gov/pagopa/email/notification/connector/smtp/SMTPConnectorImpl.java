b2b6a2a677d84b9d560ea6625e75be296e8cbe2e
Benedetta-fabbri <Benedetta11.Fabbri@emea.nttdata.com>
2023-06-20 11:57:17 +0200

    test

--- src/main/java/it/gov/pagopa/email/notification/connector/smtp/SMTPConnectorImpl.java
+++ src/main/java/it/gov/pagopa/email/notification/connector/smtp/SMTPConnectorImpl.java
@@ -3,20 +3,22 @@ package it.gov.pagopa.email.notification.connector.smtp;
 import it.gov.pagopa.email.notification.dto.smtp.MailRequest;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.util.Arrays;
 
 @Slf4j
 @Service
 public class SMTPConnectorImpl implements SMTPConnector {
 
-    private final JavaMailSender mailSender;
+    @Autowired
+    private final MailSender mailSender;
 
-    public SMTPConnectorImpl(JavaMailSender mailSender) {
+    public SMTPConnectorImpl(MailSender mailSender) {
         this.mailSender = mailSender;
     }
 
@@ -24,13 +26,12 @@ public class SMTPConnectorImpl implements SMTPConnector {
     @Override
     public void sendMessage(MailRequest mailRequest) {
         log.info("[SEND MESSAGE SMTP] Start processing message");
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false);
-        helper.setFrom(mailRequest.getFrom());
-        helper.setText(mailRequest.getContent(), true);
-        helper.setTo(InternetAddress.parse(mailRequest.getTo()));
-        helper.setSubject(mailRequest.getSubject());
-        mailSender.send(mimeMessage);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(mailRequest.getFrom());
+        simpleMailMessage.setText(mailRequest.getContent());
+        simpleMailMessage.setTo(mailRequest.getTo());
+        simpleMailMessage.setSubject(mailRequest.getSubject());
+        mailSender.send(simpleMailMessage);
         log.info("[SEND MESSAGE SMTP] End processing message");
     }
 }
